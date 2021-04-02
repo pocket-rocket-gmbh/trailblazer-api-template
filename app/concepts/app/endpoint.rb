28809999914557017224807a694ee328fcbcccd7
Nick Sutterer <apotonick@gmail.com>
2021-04-02 18:55:47 +0200

    use `insert_copy_from_domain_ctx` instead of home-made `assign_model`.

--- app/concepts/app/endpoint.rb
+++ app/concepts/app/endpoint.rb
@@ -5,6 +5,7 @@ module App::Endpoint
 
     step :strip_attributes, before: :domain_activity
 
+    Trailblazer::Endpoint::Protocol::Controller.insert_copy_from_domain_ctx!(self, {:model => :model})
     # def strip_attributes(options, params:, **)
     def strip_attributes(options, domain_ctx:, **)
       domain_ctx[:params].each {|k, v| v.strip! if v.is_a?(String)}
@@ -18,14 +19,8 @@ module App::Endpoint
   end
 
   class Adapter < Trailblazer::Endpoint::Adapter::API
-    step :assign_model
     step App::Steps::BuildPositiveCreateResult, id: :render
-
     step App::Steps::AddErrorJson, magnetic_to: :failure, Output(:success) => Track(:failure) # FIXME
-
-    def assign_model(ctx, domain_ctx:, **) # DISCUSS: make this configurable?
-      ctx[:model] = domain_ctx[:model]
-    end
 
     class List < Adapter
       step App::Steps::BuildPositiveListResult, replace: :render
