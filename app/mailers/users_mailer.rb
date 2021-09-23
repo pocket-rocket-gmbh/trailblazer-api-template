dd2abe3c1ce73bb24f6fa9a916724f676eaf89e3
Martin Braun <martinbr83@gmail.com>
2021-09-23 16:46:04 +0200

    adding user specs / introduce ActiveStorage example / customer CRUD / update endpoint with generic results

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -2,14 +2,14 @@ class UsersMailer < ApplicationMailer
 
   def send_invitation(user)
     @user = user
-    subject = "Du wurdest zur Abacuzz Service Plattform eingeladen"
+    subject = "Du wurdest zur Trailblazer Service Plattform eingeladen"
 
     mail(to: @user.email, subject: subject)
   end
 
   def send_new_password(user)
     @user = user
-    subject = "Dein neues Passwort in der Abacuzz Service Plattform"
+    subject = "Dein neues Passwort in der Trailblazer Service Plattform"
 
     mail(to: @user.email, subject: subject)
   end
